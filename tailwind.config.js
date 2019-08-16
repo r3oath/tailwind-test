a1154fef4db499e1c22e7055c415706272180000
Tristan Strathearn <tristan.strathearn@khemistry.com.au>
2019-08-16 15:49:37 +1000

    Sets up repo with initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        '80-perc': '80%',
+      },
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  },
+  plugins: []
+}
